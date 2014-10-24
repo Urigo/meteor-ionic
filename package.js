a740ffb7ee95d1766621f1f41f42055dc95f2f2f
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-10-24 12:06:13 +0300

    Update dependencies to include @netanelgilad  Cordova fix of angular-meteor

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   summary: "Ionic framework packaged for Meteor.",
-  version: "0.0.3",
+  version: "0.0.4",
   git: "https://github.com/Urigo/meteor-ionic.git"
 });
 
 Package.on_use(function (api, where) {
   api.versionsFrom('METEOR@0.9.0.1');
   api.use([
-      'urigo:angular@0.4.2',
-      'urigo:angular-ui-router@0.6.0',
+      'urigo:angular@0.4.8',
+      'urigo:angular-ui-router@0.6.1',
       'mquandalle:bower@0.1.11'],
     'client');
 
