cb8783403f709d60c6c8d14749a79a0b4dbae195
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-01-23 12:28:23 -0800

    Added back a weak dependency on urigo:angular

--- package.js
+++ package.js
@@ -1,11 +1,13 @@
 Package.describe({
   summary: "Ionic framework packaged for Meteor.",
-  version: "0.2.0",
+  version: "0.2.1",
   git: "https://github.com/Urigo/meteor-ionic.git"
 });
 
 Package.on_use(function (api, where) {
   api.versionsFrom('METEOR@0.9.0.1');
+
+  api.use('urigo:angular@0.6.1', 'client', {weak: true});
   api.use('mquandalle:bower@0.1.11', 'client');
 
   api.use([
